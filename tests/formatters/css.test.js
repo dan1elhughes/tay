db28dbb7c8050128e9141cbbf7bce9b721010cce
Dan Hughes <dan.hughes@mirumagency.com>
2018-05-09 12:12:29 +0100

    Add jest and basic tests

---
+++ tests/formatters/css.test.js
@@ -0,0 +1,30 @@
+const format = require('../../formatters/css');
+const tokens = require('../tokens.json');
+
+const expected = `
+:root {
+	--color-background-fill: 'grey';
+	--color-background-tile: 'white';
+	--color-text-strong: 'white';
+	--color-text-dim: 'rgba(255, 255, 255, 0.5)';
+	--spacing-large: '30px';
+	--spacing-medium: '20px';
+	--spacing-small: '10px';
+	--radius-large: '15px';
+	--radius-medium: '10px';
+	--radius-small: '5px';
+}
+`.trim();
+
+describe('css', () => {
+	const input = tokens;
+	const { content, extension } = format(input);
+
+	test('has the right extension', () => {
+		expect(extension).toEqual('.css');
+	});
+
+	test('has the right content', () => {
+		expect(content).toEqual(expected);
+	});
+});
---
+++ tests/formatters/json.test.js
@@ -0,0 +1,40 @@
+const format = require('../../formatters/json');
+const tokens = require('../tokens.json');
+
+const expected = `
+{
+	"color": {
+		"background": {
+			"fill": "grey",
+			"tile": "white"
+		},
+		"text": {
+			"strong": "white",
+			"dim": "rgba(255, 255, 255, 0.5)"
+		}
+	},
+	"spacing": {
+		"large": "30px",
+		"medium": "20px",
+		"small": "10px"
+	},
+	"radius": {
+		"large": "15px",
+		"medium": "10px",
+		"small": "5px"
+	}
+}
+`.trim();
+
+describe('css', () => {
+	const input = tokens;
+	const { content, extension } = format(input);
+
+	test('has the right extension', () => {
+		expect(extension).toEqual('.json');
+	});
+
+	test('has the right content', () => {
+		expect(content).toEqual(expected);
+	});
+});
