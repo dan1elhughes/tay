fcca5a70ef92e6b55cd95b42ff58a67b8a470e0b
Dan Hughes <dan.hughes@mirumagency.com>
2018-06-21 16:01:24 +0100

    Support multiple output directories

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const program = require('commander');
 const fs = require('fs');
 const yaml = require('js-yaml');
 const path = require('path');
-const write = require('./util/write');
+const util = require('util');
 
-const list = val => val.split(',');
+const unlink = util.promisify(fs.unlink);
+const writeFile = util.promisify(fs.writeFile);
+
+const collect = (val, memo) => {
+	memo.push(val);
+	return memo;
+};
 
 const cwd = process.cwd();
 
@@ -15,52 +21,62 @@ const supportedOutputFormats = ['json', 'css', 'scss'];
 program
 	.version(require('./package.json').version, '-v, --version')
 	.option('-i, --input <inputFile>', 'Input YAML file')
-	.option('-o, --output <outputDirectory>', 'Output directory')
+	.option('-o, --output <outputDirectory>', 'Output directory', collect, [])
 	.option(
-		'-f, --formats <list>',
+		'-f, --format <format>',
 		'Output formats, comma separated',
-		list,
-		supportedOutputFormats
+		collect,
+		[]
 	)
 	.parse(process.argv);
 
 if (!program.input) throw new Error('Input file not specified.');
-if (!program.output) throw new Error('Output file not specified.');
-if (!program.formats) throw new Error('Formats not specified.');
+if (!program.output.length) throw new Error('No output directories specified.');
+if (!program.format.length) throw new Error('No formats specified.');
 
-program.formats.forEach(format => {
+program.format.forEach(format => {
 	if (!supportedOutputFormats.includes(format)) {
 		throw new Error(
-			`"${format}" is not a supported format. Supported formats: ${supportedOutputFormats}`
+			`"${format}" is not a supported format (${supportedOutputFormats.join(
+				', '
+			)})`
 		);
 	}
 });
 
-const { input, output, formats } = program;
+const { input, output, format } = program;
 
 try {
-	const files = fs.readdirSync(path.join(cwd, output));
-
-	for (const file of files) {
-		if (file !== path.basename(input)) {
-			fs.unlinkSync(path.join(cwd, output, file));
-		}
-	}
-
 	const content = fs.readFileSync(path.join(cwd, input), 'utf-8');
 	const filename = path.basename(input, path.extname(input));
 	const tokens = yaml.safeLoad(content);
 
-	Promise.all(
-		formats.map(format => {
-			const formatter = require(`./formatters/${format}`);
-			const { extension, content } = formatter(tokens);
+	const tasks = [];
 
-			const file = path.join(cwd, output, `${filename}${extension}`);
+	output.forEach(outputDir => {
+		format.forEach(format => {
+			tasks.push(async () => {
+				const formatter = require(`./formatters/${format}`);
+				const { extension, content } = formatter(tokens);
 
-			return write(file, content);
-		})
-	).catch(console.error.bind(console));
+				const outputFileName = path.join(
+					cwd,
+					outputDir,
+					`${filename}${extension}`
+				);
+
+				try {
+					await unlink(outputFileName);
+				} catch (e) {
+					// File didn't exist, so ignore error.
+				}
+
+				return writeFile(outputFileName, content);
+			});
+		});
+	});
+
+	Promise.all(tasks.map(t => t())).catch(console.error.bind(console)); // eslint-disable-line
 } catch (e) {
 	console.error(e); // eslint-disable-line
 }
