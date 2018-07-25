f80d2b325d3612cca07363f97401825fb0a997b8
Dan Hughes <dan.hughes@mirumagency.com>
2018-07-25 15:53:46 +0100

    Replace homegrown camelcase fn with lodash

--- formatters/formatter-js.js
+++ formatters/formatter-js.js
@@ -1,13 +1,5 @@
 const flatten = require('flat');
-
-const toCamelCase = str =>
-	str
-		.split('-')
-		.map(
-			(word, i) =>
-				i === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
-		)
-		.join('');
+const toCamelCase = require('lodash/camelCase');
 
 module.exports = tokens => {
 	const flattened = flatten(tokens, { delimiter: '-' });
@@ -15,5 +7,5 @@ module.exports = tokens => {
 		.map(([key, value]) => `export const ${toCamelCase(key)} = "${value}";`)
 		.join('\n');
 
-	return { content, extension: '.js' };
+	return content;
 };
