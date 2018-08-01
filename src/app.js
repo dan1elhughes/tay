85e580401086078eadcbbd6e87539c6ec5e6c57b
Dan Hughes <dan.hughes@mirumagency.com>
2018-08-01 13:40:05 +0100

    Add tests for file writing

--- src/app.js
+++ src/app.js
@@ -15,15 +15,19 @@ module.exports.app = async ({ input, output = [], cwd } = {}) => {
 	if (!output.length) throw new Error('Output files not specified.');
 	if (!cwd) throw new Error('Unable to locate current working directory.');
 
+	const invalidOutputs = output
+		.filter(outputFile => !getMatchingFormat(outputFile))
+		.join(', ');
+
+	if (invalidOutputs.length)
+		throw new Error(`"${invalidOutputs}" not a supported file extension.`);
+
 	const content = await fs.readFile(path.join(cwd, input), 'utf-8');
 	const tokens = yaml.safeLoad(content);
 
 	const messages = await Promise.all(
 		output.map(async output => {
 			const format = getMatchingFormat(output);
-			if (!format) {
-				throw new Error(`"${output}" is not a supported file extension.`);
-			}
 
 			const formatter = require(`./formatters/formatter-${format}.js`);
 			const content = formatter(tokens);
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,28 @@
+const fs = require('fs-extra');
 const { app } = require('../src/app');
+
+const yaml = `
+---
+font: "'Open Sans', Arial, sans-serif"
+spacing:
+  large: 30px
+  medium: 20px
+  small: 10px
+`.trim();
 
 describe('app', () => {
 	const defaultSettings = {
-		input: 'fake/tokens.yaml',
-		output: ['fake/output/tokens.css'],
-		cwd: 'fake',
+		input: '.test-data/tokens.yaml',
+		output: ['.test-data/output/tokens.css'],
+		cwd: '.',
 	};
+
+	beforeEach(() => fs.outputFile('./.test-data/tokens.yaml', yaml));
+	afterEach(() => fs.remove('./.test-data'));
+
+	test('throws with no settings', async () => {
+		await expect(app()).rejects.toThrow('Input file not specified.');
+	});
 
 	test('throws with missing input', async () => {
 		const { input, ...settings } = defaultSettings;
@@ -23,4 +40,25 @@ describe('app', () => {
 			'Unable to locate current working directory.'
 		);
 	});
+
+	test('throws with invalid format', async () => {
+		const settings = {
+			...defaultSettings,
+			output: ['.test-data/output/tokens.fail'],
+		};
+
+		await expect(app(settings)).rejects.toThrow(
+			'".test-data/output/tokens.fail" not a supported file extension.'
+		);
+	});
+
+	test('writes single file successfully', async () => {
+		await app(defaultSettings);
+
+		const exists = await fs.exists(defaultSettings.output[0]);
+
+		expect(exists).toBe(true);
+	});
+
+	test('writes multiple files successfully', async () => {});
 });
