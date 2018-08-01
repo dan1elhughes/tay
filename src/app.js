1f292d091eacaff0dfdda5defb60acb5431746de
Dan Hughes <dan.hughes@mirumagency.com>
2018-08-01 10:57:15 +0100

    Throw on missing cwd

--- src/app.js
+++ src/app.js
@@ -10,9 +10,10 @@ module.exports.formats = formats;
 const getMatchingFormat = filename =>
 	formats.find(format => filename.endsWith(format));
 
-module.exports.app = async ({ input, output = [], cwd }) => {
+module.exports.app = async ({ input, output = [], cwd } = {}) => {
 	if (!input) throw new Error('Input file not specified.');
 	if (!output.length) throw new Error('Output files not specified.');
+	if (!cwd) throw new Error('Unable to locate current working directory.');
 
 	const content = await fs.readFile(path.join(cwd, input), 'utf-8');
 	const tokens = yaml.safeLoad(content);
